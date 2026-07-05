ee62bb97f09c6b26e00728a0bf385b78618a94b4
Sam Fonti <energyarrow@live.com>
2026-07-05 18:03:55 +0200

    v1.0 - App icon and branding

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -39,7 +39,7 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("🌱 LessMeatCalendar"),
+        title: const Text("🌱 Less Meat Calendar"),
         centerTitle: true,
       ),
       body: Column(
